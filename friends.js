659b6e945a641fe17ab7d9c0349056a155aae815
Alex Alexander <a.alexander62442@gmail.com>
2022-11-09 13:10:57 -0700

    Add array method

--- friends.js
+++ friends.js
@@ -1,4 +1,8 @@
-var arrays = ["Dave", "Tyler", "Chris", "Tim", "Bill"]
-console.log(arrays[2])
-console.log(arrays[4])
-console.log(arrays[0])
+// var arrays = ["Dave", "Tyler", "Chris", "Tim", "Bill"]
+// console.log(arrays[2])
+// console.log(arrays[4])
+// console.log(arrays[0])
+
+
+var places = ["Prague", "Paris", "Rome", "Tokyo", "Naples"]
+console.log(places.pop())
